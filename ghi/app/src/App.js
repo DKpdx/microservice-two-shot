d95ddf298014ecf6a875ec83ede851ceb44b8e7e
danny mitchell <dkmtchll@gmail.com>
2023-03-03 17:05:38 -0600

    finished project

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,16 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-<<<<<<< HEAD
 import ShoeList from './ShoeList';
 import ShoeForm from './CreateShoeForm';
 
-=======
 import HatForm from './HatForm';
 import HatsList from "./HatsList"
 
 
->>>>>>> main
 function App(props) {
   return (
     <BrowserRouter>
@@ -18,19 +15,17 @@ function App(props) {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-<<<<<<< HEAD
           <Route path="shoes">
             <Route path="new" element={<ShoeForm />} />
           </Route>
           <Route path="shoes">
             <Route path="" element={<ShoeList shoes={props.shoes}/>} />
-=======
+          </Route>
           <Route path="hats">
             <Route path="new"  element={<HatForm />} />
           </Route>
           <Route path="hats">
             <Route path="" element={<HatsList hats={props.hats} />} />
->>>>>>> main
           </Route>
         </Routes>
       </div>
